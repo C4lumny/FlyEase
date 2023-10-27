37b6e79b1f02dffc7201db90e6d0284ca93116cc
C4lumny <ospinonathan@gmail.com>
2023-10-27 14:49:51 -0500

    Header listo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,19 +3,19 @@ import logo from "../assets/Logo_FlyEase.png";
 
 export function Header() {
   return (
-    <header className="header">
+    <header className="flex justify-center h-[120px]">
       {/* Logo divs */}
-      <div className="logo">
-        <img src={logo} alt="FlyEase-logo" />
-        <p className="lowfont">FlyEase</p>
+      <div className="flex gap-2.5 m-5 items-center w-[45%]">
+        <img src={logo} alt="FlyEase-logo" className="w-20 cursor-pointer hover:rotate-[360deg] ease-in-out duration-[1s]" />
+        <p className="font-bold text-xl text-[#ff3e41] cursor-pointer hover:text-[#0070ff] transition duration-[0.25s]">FlyEase</p>
       </div>
 
       {/* Nav divs */}
-      <div className="flyease-navs">
-        <ul>
-          <li>Inicio</li>
-          <li>Vuelos</li>
-          <li>Acerca de</li>
+      <div className="flex w-1/2 items-center justify-center">
+        <ul className="flex justify-center gap-16">
+          <li className="list-none text-xl font-bold cursor-pointer text-[#ff3e41] hover:text-[#0070ff] duration-[0.25s]">Inicio</li>
+          <li className="list-none text-xl font-bold cursor-pointer text-[#ff3e41] hover:text-[#0070ff] duration-[0.25s]">Vuelos</li>
+          <li className="list-none text-xl font-bold cursor-pointer text-[#ff3e41] hover:text-[#0070ff] duration-[0.25s]">Acerca de</li>
         </ul>
       </div>
     </header>
