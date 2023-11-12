95ba4841b8f31cb255f5b52ff6fd34de4e5ec5a4
C4lumny <ospinonathan@gmail.com>
2023-11-11 19:12:59 -0500

    Update useFetch hook endpoint URL.

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,19 +1,18 @@
 import { useState, useEffect } from "react";
 
-export function useFetch(endpoint){
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
- 
-    useEffect(() => {
-      setLoading(true);
-      fetch(`https://www.flyease.somee.com/FlyEaseApi${endpoint}`)
-        .then((response) => response.json())
-        .then((data) => setData(data.response))
-        .catch((error) => setError(error))
-        .finally(() => setLoading(false));
+export function useFetch(endpoint) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    }, [endpoint]);
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://flyeasewebapi.azurewebsites.net/FlyEaseApi${endpoint}`)
+      .then((response) => response.json())
+      .then((data) => setData(data.response))
+      .catch((error) => setError(error))
+      .finally(() => setLoading(false));
+  }, [endpoint]);
 
-    return { data , loading, error};
-} 
+  return { data, loading, error };
+}
