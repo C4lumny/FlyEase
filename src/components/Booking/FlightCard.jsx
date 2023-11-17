d910e2d7e32a6777f483dce5f3e061369c9d35fe
C4lumny <ospinonathan@gmail.com>
2023-11-16 21:03:52 -0500

    Fix formatting in FlightCard component

--- src/components/Booking/FlightCard.jsx
+++ src/components/Booking/FlightCard.jsx
@@ -14,7 +14,8 @@ export function FlightCard(vuelo) {
           <div className="flex items-center justify-between w-9/12">
             <div>
               <p className="text-4xl font-semibold">
-                {fechaSalida.getHours().toString().padStart(2, '0')}:{fechaSalida.getMinutes().toString().padStart(2, '0')}
+                {fechaSalida.getHours().toString().padStart(2, "0")}:
+                {fechaSalida.getMinutes().toString().padStart(2, "0")}
               </p>
               <p className="font-semibold">BOG</p>
             </div>
@@ -23,7 +24,8 @@ export function FlightCard(vuelo) {
             </div>
             <div>
               <p className="text-4xl font-semibold">
-                {fechaLlegada.getHours().toString().padStart(2, '0')}:{fechaLlegada.getMinutes().toString().padStart(2, '0')}
+                {fechaLlegada.getHours().toString().padStart(2, "0")}:
+                {fechaLlegada.getMinutes().toString().padStart(2, "0")}
               </p>
               <p className="font-semibold">VUP</p>
             </div>
@@ -31,7 +33,7 @@ export function FlightCard(vuelo) {
           <div className="w-3/12">
             <p className="text-zinc-500">Precio:</p>
             <div>
-              COP <span className="text-4xl font-semibold">{vueloInfo.preciovuelo.toLocaleString('es-ES')}</span>
+              COP <span className="text-4xl font-semibold">{vueloInfo.preciovuelo.toLocaleString("es-ES")}</span>
             </div>
           </div>
         </div>
@@ -42,7 +44,7 @@ export function FlightCard(vuelo) {
             alt=""
             className="h-full w-full object-cover rounded-2xl hover:rotate-180"
           />
-          <span className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-white to-transparent"></span>
+          <span className="absolute top-0 left-0 w-full h-full bg-gradient-to-r from-white via-[rgba(255,255,255,0.1)] to-transparent"></span>{" "}
         </div>
       </div>
     </div>
