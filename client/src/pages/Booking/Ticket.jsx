2aba8b53a3428d281544e8303a9621eae1ba1756
C4lumny <ospinonathan@gmail.com>
2023-11-20 19:11:38 -0500

    Delete unused files and configurations

---
+++ client/src/pages/Booking/Ticket.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
+import axios from "axios";
+//Imports de componentes
+import { FlightHeader } from "../../components/Booking/FlightHeader";
+import { ClientInfoCard } from "../../components/Booking/ClientInfoCard.jsx";
+import { Footer } from "../../components/Footer-flyease";
+import PlaneSeats from "../../components/Booking/PlaneSeats.jsx";
+//Imports de contextos
+import { useFlightContext } from "../../context/FlightProvider.jsx";
+import { useSelectedFlightContext } from "../../context/SelectedFlight.jsx";
+import { useClientContext } from "../../context/ClientProvider.jsx";
+import { useSeatsContext } from "../../context/SeatsProvider.jsx";
+//Imports de funciones
+import { useInsertBoleto } from "../../api/useInsertBoleto.js";
+
+export function Ticket() {
+  const [preferenceId, setPreferenceId] = useState(null);
+  const { flightInfo } = useFlightContext();
+  const { SelectedflightInfo } = useSelectedFlightContext();
+  const { clientInfo } = useClientContext();
+  const { selectedSeat } = useSeatsContext();
+  const insertBoleto = useInsertBoleto();
+
+  initMercadoPago("TEST-b662e221-e1ef-4721-b5c0-763a46d5e94b");
+
+  const createPreference = async () => {
+    try {
+      const response = await axios.post("http://localhost:8080/create_preference", {
+        description: "Insano",
+        price: 100000,
+        quantity: 1,
+      });
+
+      const { id } = response.data;
+      return id;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleBuy = async () => {
+    const id = await createPreference();
+    if (id) {
+      setPreferenceId(id);
+    }
+  };
+
+  console.log(flightInfo);
+  console.log(SelectedflightInfo);
+  console.log(clientInfo);
+  console.log(selectedSeat);
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <FlightHeader flightInfo={flightInfo} />
+      <div className="ml-24 mr-48 my-16">
+        <div className=" grid grid-cols-2 gap-4">
+          <div className="ml-16">
+            <PlaneSeats idVuelo={SelectedflightInfo.idvuelo} />
+          </div>
+          <div className="flex flex-col justify-center gap-5">
+            <div className="text-2xl font-bold">Información del cliente</div>
+            {selectedSeat ? <ClientInfoCard clientInfo={clientInfo} selectedSeat={selectedSeat.posicion} /> : null}
+          </div>
+        </div>
+        <div className="justify-end flex w-full">
+          <button
+            className="px-14 py-2 mt-10 bg-zinc-700 rounded-xl text-white"
+            // onClick={() => insertBoleto(clientInfo, SelectedflightInfo, selectedSeat)}
+            onClick={handleBuy}
+          >
+            Comprar
+          </button>
+          {preferenceId && <Wallet initialization={{ preferenceId, redirectMode: 'modal' }} />}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
---
+++ server/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mercadopago = require("mercadopago");
+
+app.use(express.json());
+app.use(cors());
+
+mercadopago.configure({
+  access_token: "TEST-8080658544283714-112017-ed11e9efcbb3afa8073cc317b452e862-225213955",
+});
+
+app.get("/", function (req, res) {
+  res.send("El servidor de mercado pago funciona correctamente");
+});
+
+app.post("/create_preference", (req, res) => {
+  let preference = {
+    items: [
+      {
+        title: req.body.description,
+        unit_price: Number(req.body.price),
+        quantity: Number(req.body.quantity),
+      },
+    ],
+    back_urls: {
+      success: "http://localhost:5173/booking/details/ticket",
+      failure: "http://localhost:5173/booking/details/ticket",
+      pending: "",
+    },
+    auto_return: "approved",
+  };
+
+  mercadopago.preferences
+    .create(preference)
+    .then(function (response) {
+      res.json({
+        id: response.body.id,
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+app.listen(8080, () => {
+  console.log("Server running on port 8080");
+});
